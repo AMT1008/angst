3246a382396bbfd9915b19e4afb2e276111f01ab
AMT1008 <analiesetorres@icloud.com>
2025-03-25 10:05:54 -0400

    text, readme

---
+++ main.js
@@ -0,0 +1,8 @@
+document.addEventListener('scroll', function() {
+    const footer = document.querySelector('footer');
+    if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
+        footer.classList.add('visible');
+    } else {
+        footer.classList.remove('visible');
+    }
+});
